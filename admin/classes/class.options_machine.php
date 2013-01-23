d35184d9776965dde642326a960c44e7970b3a69
partnuz <partnuz@gmail.com>
2013-01-23 19:23:23 +0100

    Update admin/classes/class.options_machine.php
    Removed because of bug

--- admin/classes/class.options_machine.php
+++ admin/classes/class.options_machine.php
@@ -468,16 +468,6 @@ class Options_Machine {
 				
 				break;
 				
-				// google font field
-				case 'select_google_font':
-					$output .= '<div class="select_wrapper">';
-					$output .= '<select class="select of-input google_font_select" name="'.$value['id'].'" id="'. $value['id'] .'">';
-					foreach ($value['options'] as $select_key => $option) {			
-						$output .= '<option value="'.$select_key.'" ' . selected($data[$value['id']], $option, false) . ' />'.$option.'</option>';	 
-					 } 
-					$output .= '</select></div>';
-					$output .= '<p class="google_font_preview">0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz</p>';
-				break;
 			
 			}
 			
