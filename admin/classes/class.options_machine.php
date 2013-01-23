d6402cdca81db2eeb6e6542dfffb7a5066f709be
partnuz <partnuz@gmail.com>
2013-01-23 18:33:44 +0100

    Update admin/classes/class.options_machine.php
    google font field in php

--- admin/classes/class.options_machine.php
+++ admin/classes/class.options_machine.php
@@ -467,6 +467,17 @@ class Options_Machine {
 					$output .= '<a href="#" id="of_import_button" class="button" title="Restore Options">Import Options</a>';
 				
 				break;
+				
+				// google font field
+				case 'select_google_font':
+					$output .= '<div class="select_wrapper">';
+					$output .= '<select class="select of-input google_font_select" name="'.$value['id'].'" id="'. $value['id'] .'">';
+					foreach ($value['options'] as $select_key => $option) {			
+						$output .= '<option value="'.$select_key.'" ' . selected($data[$value['id']], $option, false) . ' />'.$option.'</option>';	 
+					 } 
+					$output .= '</select></div>';
+					$output .= '<p class="google_font_preview">0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz</p>';
+				break;
 			
 			}
 			
