995ef0e82a2adbbefa334943b295f37358c271f1
Jonah Dahlquist <jonah@nucleussystems.com>
2013-03-06 17:46:24 -0800

    Created functions file for data filters, added to functions load file

---
+++ admin/functions/functions.filters.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * SMOF Option filters
+ *
+ * @package     WordPress
+ * @subpackage  SMOF
+ * @since       1.4.0
+ * @author      Jonah Dahlquist
+ */
--- admin/functions/functions.load.php
+++ admin/functions/functions.load.php
@@ -12,3 +12,4 @@ require_once( ADMIN_PATH . 'functions/functions.interface.php' );
 require_once( ADMIN_PATH . 'functions/functions.options.php' );
 require_once( ADMIN_PATH . 'functions/functions.admin.php' );
 require_once( ADMIN_PATH . 'functions/functions.mediauploader.php' );
+require_once( ADMIN_PATH . 'functions/functions.filters.php' );
