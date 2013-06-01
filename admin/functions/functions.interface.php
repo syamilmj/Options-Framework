c88a7f50fa2a06fe5e2504ea5e6ec54230fcba9b
Andrei <smartik89@gmail.com>
2013-06-01 16:38:32 +0300

    Revert "Two more hooks, styles and scripts."
    This reverts commit 77700707c2b6fcfa819c5730ac888edc4142d0fa.

--- admin/functions/functions.interface.php
+++ admin/functions/functions.interface.php
@@ -96,7 +96,6 @@ function of_style_only(){
 		wp_register_style( 'wp-color-picker', ADMIN_DIR . 'assets/css/color-picker.min.css' );
 	}
 	wp_enqueue_style( 'wp-color-picker' );
-	do_action('of_style_only_after');
 
 }	
 
@@ -136,8 +135,6 @@ function of_load_only()
 	
 	if ( function_exists( 'wp_enqueue_media' ) )
 		wp_enqueue_media();
-
-	do_action('of_load_only_after');
 
 }
 
