bc2cfc73ca1a858081b0f1922dfcc5a0bad923c4
Dovy Paukstys <dovy@rtcollab.com>
2013-05-31 19:21:01 -0600

    It was my fault! Fixed the padding issue.

--- admin/classes/class.options_machine.php
+++ admin/classes/class.options_machine.php
@@ -493,7 +493,7 @@ class Options_Machine {
 					
 					$output .= '<div class="backup-box">';
 					$output .= '<div class="instructions">'.$instructions."\n";
-					$output .= '<p><strong>'. __('Last Backup : ').'<span class="backup-log">'.$log.'</span></strong>'."\n";
+					$output .= '<p><strong>'. __('Last Backup : ').'<span class="backup-log">'.$log.'</span></strong></p></div>'."\n";
 					$output .= '<a href="#" id="of_backup_button" class="button" title="Backup Options">Backup Options</a>';
 					$output .= '<a href="#" id="of_restore_button" class="button" title="Restore Options">Restore Options</a>';
 					$output .= '</div>';
