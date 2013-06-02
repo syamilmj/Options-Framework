041f81041cd754fb95dc1999138588c7d283237e
Aristeides Stathopoulos <aristath@gmail.com>
2013-06-02 20:49:13 +0300

    Update template-debug.php

--- template-debug.php
+++ template-debug.php
@@ -3,12 +3,9 @@
 Template Name: Debug
 */
 get_header();
-?>
-		<pre>
-		<?php 
-		$smof_data_r = print_r($smof_data, true); 
-		$smof_data_r_sans = htmlspecialchars($smof_data_r, ENT_QUOTES); 
-		echo $smof_data_r_sans; ?>
-		</pre>
-	
-<?php get_footer(); ?>
+echo '<pre>';
+$smof_data_r = print_r($smof_data, true);
+$smof_data_r_sans = htmlspecialchars($smof_data_r, ENT_QUOTES);
+echo $smof_data_r_sans;
+echo '</pre>';
+get_footer();
