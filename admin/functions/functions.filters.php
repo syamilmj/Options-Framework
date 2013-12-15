52d55bdbca635d21c4c57cd4e36ac1eb2f1505fd
Adityo Ij <kenzi.white@gmail.com>
2013-12-15 10:59:50 +0700

    Update functions.filters.php
    Add same line code like code of bottom, so custom background is working now..

--- admin/functions/functions.filters.php
+++ admin/functions/functions.filters.php
@@ -23,7 +23,7 @@ function of_filter_save_media_upload($data) {
     if(!is_array($data)) return $data;
     
     foreach ($data as $key => $value) {
-        if (is_string($value)) {
+        if (is_string($value) && preg_match("/\[site_url(_url_secure)?\]/", $value)) {
             $data[$key] = str_replace(
                 array(
                     site_url('', 'http'),
